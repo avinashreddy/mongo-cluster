cd1ceea9ca7c32514ba7aadf95edfbf421bc245a
Avinash Palicharla <avinash.palicharla@matrixapps.com>
2020-04-14 15:18:07 -0400

    ask-vault-pass

--- setup-hosts.sh
+++ setup-hosts.sh
@@ -6,5 +6,5 @@
 # ssh-keygen -R 10.113.1.14
 # ssh-keygen -R 10.113.1.15
 
-ansible-playbook -b playbook-configure-hosts.yaml --e target_hosts=mongo
+ansible-playbook -b playbook-configure-hosts.yaml --ask-vault-pass  --e target_hosts=mongo
 
